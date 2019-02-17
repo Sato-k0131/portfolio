ba309458da5de9712c7f49d3ffbd52ae760e8b69
Sato-k0131 <s.246.at@gmail.com>
2019-02-17 03:51:00 +0000

    20190217 スクロール実装

--- js/scroll.js
+++ js/scroll.js
@@ -5,20 +5,24 @@
 
 // SCROLL
 $(function(){
-   // #で始まるアンカーをクリックした場合に処理
-   $('a[href^=#]').click(function() {
-      // スクロールの速度
-      var speed = 400; // ミリ秒
-      // アンカーの値取得
-      var href= $(this).attr("href");
-      // 移動先を取得
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      // 移動先を数値で取得
-      var position = target.offset().top;
-      // ずらしたいY軸方向の値。
-      var OFFSET = 90;
-      // スムーススクロール
-      $('body,html').animate({scrollTop:position-OFFSET}, speed, 'swing');
-      return false;
-   });
+	var headerHeight = $('#header_in').outerHeight();
+	var urlHash = location.hash;
+	// URLにハッシュが含まれる場合
+	if(urlHash) {
+		$('body,html').stop().scrollTop(0);
+		setTimeout(function(){
+			var target = $(urlHash);
+			var position = target.offset().top - headerHeight;
+			$('body,html').stop().animate({scrollTop:position}, 500, 'swing');
+		}, 100);
+	}
+	// 該当ページのページ内リンクの場合
+	$('a[href^="#"]').click(function() {
+		var href= $(this).attr("href");
+		var target = $(href);
+		var position = target.offset().top - headerHeight;
+		$('body,html').stop().animate({scrollTop:position}, 500, 'swing');
+		return false;
+	});
 });
+	
